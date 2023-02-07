6b264381129cf747314ec248e8146b1b1a46acb2
Crowdbotics <engineering@crowdbotics.com>
2023-02-07 00:09:05 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/late_sunset_dev_61595/MainActivity.java
@@ -0,0 +1,15 @@
+package com.late_sunset_dev_61595;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "late_sunset_dev_61595";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "late-sunset-dev-61595",
+  url: "https://late-sunset-dev-61595.botics.co",
+  api: "https://late-sunset-dev-61595.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
